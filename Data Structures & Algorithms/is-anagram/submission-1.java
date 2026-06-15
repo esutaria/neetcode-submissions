bc442d9a1435343e4590a8abec43ff4ab1a54214
esutaria <eeshasutaria@gmail.com>
2026-06-15 10:35:10 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()){
+            return false;
+        }
+        char[] s_char = s.toCharArray();
+        char[] t_char = t.toCharArray();
+
+        Arrays.sort(s_char);
+        Arrays.sort(t_char);
+
+        if (!Arrays.equals(s_char,t_char)){
+            return false;
+        }
+        return true;
+    }
+}
